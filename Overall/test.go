829d9c9d064f6e686377a7bd9ef26453117a0da7
x33lyS <71019197+x33lyS@users.noreply.github.com>
2020-12-02 18:59:29 +0100

    with all settings

---
+++ Overall/test.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("\033[36m"+"\033[7m"+"okok")
+	fmt.Println("\033[0m")
+	fmt.Println("\033[36m"+"\033[40m"+"okok")
+	fmt.Println("\033[36m"+"\033[41m"+"okok")
+	fmt.Println("\033[36m"+"\033[42m"+"okok")
+	fmt.Println("\033[36m"+"\033[43m"+"okok")
+	fmt.Println("\033[36m"+"\033[44m"+"okok")
+	fmt.Println("\033[36m"+"\033[45m"+"okok")
+	fmt.Println("\033[36m"+"\033[46m"+"okok")
+	fmt.Println("\033[36m"+"\033[100m"+"okok")
+	fmt.Println("\033[36m"+"\033[101m"+"okok")
+	fmt.Println("\033[36m"+"\033[105m"+"okok")
+	fmt.Println("\033[36m"+"\033[106m"+"okok")
+
+
+}
